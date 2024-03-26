f7993b7a7ab97f5579e09d5b867c9d1bf3bae75c
briankaboyi <brianmkaboyi@gmail.com>
2024-03-26 21:07:32 +0300

    add future

--- lib/widgets/shimmer_widget.dart
+++ lib/widgets/shimmer_widget.dart
@@ -4,15 +4,61 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 
 class ShimmerWidget extends GetView {
+  var isVisible=false;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
+    Future.delayed(Duration(seconds: 5),() {
+      isVisible = true;
+    },);
+    // TODO: create container to be displayed
+    return isVisible==false? Container(
       height: 100,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
           color: Color(0xffe0e0e0,
             
+          )),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8),
+            child: FadeShimmer.round(
+              highlightColor: Color(0xffF1F1F1),
+              baseColor: Color(0xffa6a8aB),
+              size: 60,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FadeShimmer(
+                height: 10,
+                width: 230,
+                radius: 4,
+                highlightColor: Color(0xffF1F1F1),
+                baseColor: Color(0xffa6a8aB),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FadeShimmer(
+                height: 10,
+                width: 230,
+                radius: 4,
+                highlightColor: Color(0xffF1F1F1),
+                baseColor: Color(0xffa6a8aB),
+              ),
+            ],
+          )
+        ],
+      ),
+    ) :  Container(
+      height: 100,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Color(0xffe0e0e0,
+
           )),
       child: Row(
         children: [
